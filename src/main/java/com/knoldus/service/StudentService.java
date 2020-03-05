98bda79c05ff4548bb25b1cf662f6f41d3097e48
yamini-ban <yaminibansal7@gmail.com>
2020-03-05 10:22:48 +0530

    logic corrected for students with given name and no secondary address

--- src/main/java/com/knoldus/service/StudentService.java
+++ src/main/java/com/knoldus/service/StudentService.java
@@ -4,19 +4,23 @@ import com.knoldus.repository.Student;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentService {
 
     public List<Student> getStudentsWithNoSecondaryAddress(List<Student> listOfStudents) {
-        return listOfStudents.stream().filter(student -> student.getAddress().getSecondaryAddress().isPresent()).collect(Collectors.toList());
+        return listOfStudents
+                .stream()
+                .filter(student -> student.getAddress().getSecondaryAddress().isPresent())
+                .collect(Collectors.toList());
     }
 
-    public List<Student> getStudentsWithInitialOfUserAndNoSecAddress(List<Student> listOfStudents, String initialAlphabet) {
-        List<Student> studentList = listOfStudents
+    public List<Student> getStudentsWithInitialOfUserAndNoSecAddress(
+            List<Student> listOfStudents, String initialAlphabet) {
+        Stream<Student> studentStream = listOfStudents
                 .stream()
-                .filter(student -> student.getName().toLowerCase().startsWith(initialAlphabet.toLowerCase()))
-                .collect(Collectors.toList());
-        return getStudentsWithNoSecondaryAddress(studentList);
+                .filter(student -> student.getName().toLowerCase().startsWith(initialAlphabet.toLowerCase()));
+        return studentStream.filter(student -> !(student.getAddress().getSecondaryAddress().isPresent())).collect(Collectors.toList());
     }
 
 }
